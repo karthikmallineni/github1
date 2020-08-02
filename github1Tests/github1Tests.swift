73b3df8a6b613a03abf0e43a9dd656d83da8ac81
Karthik <mkarthik.appdev@gmail.com>
2020-08-02 22:37:29 +0530

    Initial Commit

---
+++ github1Tests/github1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  github1Tests.swift
+//  github1Tests
+//
+//  Created by Administrator on 02/08/20.
+//  Copyright © 2020 Administrator. All rights reserved.
+//
+
+import XCTest
+@testable import github1
+
+class github1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
